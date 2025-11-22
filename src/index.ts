e4cced499a3722c606893b35ebddbf802dedad32
Shlok-Dubey <dubeyshlok12@gmail.com>
2025-11-23 00:30:52 +0530

    vercell 4

--- src/index.ts
+++ src/index.ts
@@ -38,5 +38,5 @@ app.get('/',(req,res)=>{
 //   });
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  app(req as any, res as any);
+  return app(req as any, res as any);
 }
